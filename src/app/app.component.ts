7787696d0f17647cfe49c7c30d1ed387cb04f6cd
Mayur <mayurgsh@gmail.com>
2025-08-28 17:02:24 +0530

    search function implemented without negative and space allocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { FormGroup } from '@angular/forms';
+import { fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,45 +9,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  step: number = 1;
-  loginForm: FormGroup;
-  contactForm: FormGroup;
+  title="equet"
+ employeeDetailform!:FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-    this.contactForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required]
-    });
-  }
+    searchText: string = '';
+    filteredResults: any[] = [];
+    allEmployees: any[] = [];
+      invalidSearch: boolean = false;
 
-  ngOnInit(): void {}
+    constructor(private _empservice: EmployeeService) {}
 
-  nextStep() {
-    if (this.step < 3) {
-      this.step++;
+    ngOnInit(): void {
+      // Fetch all employees for search
+      this._empservice.getEmployeeData().subscribe((data: any[]) => {
+        this.allEmployees = data;
+        this.filteredResults = data;
+      });
     }
-  }
 
-  prevStep() {
-    if (this.step > 1) {
-      this.step--;
+    onSearchChange(event: any): void {
+      const value = event.target.value;
+      // Disallow spaces and negative characters
+        this.invalidSearch = /\s|-/g.test(value);
+        if (this.invalidSearch) {
+          this.searchText = value;
+          this.filteredResults = [];
+          return;
+        }
+      this.searchText = value;
+      this.filteredResults = this.allEmployees.filter(emp =>
+        emp.name.toLowerCase().includes(value.toLowerCase())
+      );
     }
-  }
+ 
 
-  submitLogin() {
-    if (this.loginForm.valid) {
-      this.nextStep();
-    }
-  }
+ 
+ 
+   
+  
 
-  submitContact() {
-    if (this.contactForm.valid) {
-      this.nextStep();
-    }
-  }
- }
 
+}
